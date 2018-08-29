6d3719643c9b0dc4f768db573fe067dffad5d87b
Mejía Bedoya <melinamejia95@gmail.com>
2018-08-29 15:37:21 -0500

    Saving theme in localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { SettingsService } from './services/settings.service';
 
 
 @NgModule({
@@ -27,7 +28,7 @@ import { RegisterComponent } from './login/register.component';
     PagesModule,
     FormsModule
   ],
-  providers: [],
+  providers: [SettingsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
