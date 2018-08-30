7de459e711f7613a7b242a82c36e20d52e8e40c7
Mejía Bedoya <melinamejia95@gmail.com>
2018-08-30 14:42:23 -0500

    COnfigurations using Observables and Promises

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -13,6 +13,8 @@ import { PAGES_ROUTES } from './pages.routes';
 import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 @NgModule({
@@ -23,7 +25,9 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
--- src/app/shared/breadcrums/breadcrums.component.ts
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrums',
   templateUrl: './breadcrums.component.html',
-  styles: []
+  styles: [] 
 })
 export class BreadcrumsComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+
+  constructor( private router: Router, private title: Title, private meta: Meta ) {
+      
+    this.getDataRoute()
+    .subscribe( data => {
+      this.titulo = data.titulo;
+      this.title.setTitle(this.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
+      this.meta.updateTag(metaTag);
+    });
+
+   }
 
   ngOnInit() {
+  }
+
+  getDataRoute() {
+
+    return this.router.events.pipe(
+      filter( evento => evento instanceof ActivationEnd ),
+      filter( (evento: ActivationEnd) => evento.snapshot. firstChild === null ),
+      map( (evento: ActivationEnd) => evento.snapshot.data )
+    )
+
   }
 
 }
