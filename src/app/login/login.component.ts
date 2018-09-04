8cb4c3b46d8942fa19d6b44f6a16ac9c173a25b6
Mejía Bedoya <melinamejia95@gmail.com>
2018-09-04 14:31:21 -0500

    Login and Google Authentication functions added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -10,14 +14,53 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router ) { }
+  recuerdame: boolean = false;
+  email: string;
+  auth2: any;
+
+  constructor( public router: Router, public _usuarioService: UsuarioService ) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+    this.email = localStorage.getItem('email') || '';
+    if (this.email.length > 1){
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    this.router.navigate([ '/dashboard' ]);
+  googleInit() {
+
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '719874225079-30qvag9fildof4vrv3plj44aeq7a2770.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin( document.getElementById('btnGoogle') );
+    });
+
+  }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler( element, {}, googleUser => {
+      //let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+      this._usuarioService.loginGoogle( token ).subscribe( () => window.location.href = '#/dashboard'); //Se hizo uso del window.location ya que el router recargaba la página y el template se desconfiguraba
+    });
+
+  }
+
+  ingresar( form: NgForm ) {
+
+    if ( form.invalid ) {
+      return;
+    }
+
+    let usuario = new Usuario( null, form.value.email, form.value.password )
+     this._usuarioService.login( usuario, form.value.recuerdame ).subscribe(data => this.router.navigate(['/dashboard']));
+
   }
 
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -2,20 +2,58 @@ import { Injectable } from '@angular/core';
 import { Usuario } from '../../models/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
-//import  'rxjs/operator/map';
-import { from } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { pipe } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
+  usuario: Usuario;
+  token: string;
+
   constructor( public http: HttpClient) { }
 
+  guardarStorage( id: string, token: string, usuario: Usuario ) {
+    
+    localStorage.setItem('id', id);
+    localStorage.setItem('token', token);
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+    this.usuario = usuario;
+    this.token = id;
+
+  }
+
+  loginGoogle( token: string ) {
+
+    let url = URL_SERVICIOS + '/login/google';
+    return this.http.post( url, { token } )
+      .pipe(map( (data: any) => {
+        this.guardarStorage(data.id, data.token, data.usuario);
+        return true;
+      }));
+
+  }
+
+  login( usuario: Usuario, recordar: boolean = false ) {
+
+    if ( recordar ) {
+      localStorage.setItem('email', usuario.email);
+    }  else {
+      localStorage.removeItem('email');
+    }
+    
+    let url = URL_SERVICIOS + '/login';
+    return this.http.post( url, usuario )
+      .pipe(map( (data: any) => {
+        this.guardarStorage(data.id, data.token, data.usuario);
+        return true;
+      }))
+
+  }
+
   crearUsuario( usuario: Usuario ) {
+
     let url = URL_SERVICIOS + '/usuario';
     return this.http.post( url, usuario )
      .pipe(map( (data: any) => {
@@ -24,6 +62,7 @@ export class UsuarioService {
         return data.usuario;
       })
     )
+
   }
 
 }
